e34da3266deb7b26dd8dbd1ef67fb48f4073bf80
TUTULROY <tutulroyjoy@gmail.com>
2024-03-25 01:42:41 +0600

    Completed project

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+    return (
+        <div className="text-5xl text-center">
+            <h2>Home page Banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
